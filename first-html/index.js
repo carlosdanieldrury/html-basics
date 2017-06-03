aa3951b5a10e92e80084f4db3a6b89aa318a0812
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2017-06-03 14:29:57 -0300

    Added javascript

---
+++ first-html/index.js
@@ -0,0 +1,16 @@
+console.log('Hi There!');
+
+/*
+function sleep (miliseconds) {
+    let startTime = Date.now();
+    while(Date.now() < startTime + miliseconds);
+}
+
+sleep(3000);
+
+console.log('Hi there after sleep');
+*/
+
+for(let i=0; i<10; i++) {
+    console.log('Hi Number ' + i);
+}
